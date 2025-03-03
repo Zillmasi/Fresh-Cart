fbd9040cbabb057ab30183df24192331d5b6a054
Ahmed Adel <ameddel70@gmail.com>
2025-03-03 12:09:45 +0200

    Fresh Cart

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,21 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'details/:id',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'categoriesDetails/:id',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'brandsDetails/:id',
-    renderMode: RenderMode.Prerender
-  },
-
-
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
